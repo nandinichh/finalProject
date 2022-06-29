3e85b84dfce62094a6b3685dd3caf2b0ce9d43b1
Daniela Ruano <ruthdanielaruano@gmail.com>
2022-06-29 11:11:50 -0600

    segues

---
+++ finalProject/northAmerica0ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  northAmerica0ViewController.swift
+//  finalProject
+//
+//  Created by Daniela Ruano on 29/06/22.
+//
+
+import UIKit
+
+class northAmerica0ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ finalProject/southAmerica0ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  southAmerica0ViewController.swift
+//  finalProject
+//
+//  Created by Daniela Ruano on 29/06/22.
+//
+
+import UIKit
+
+class southAmerica0ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
