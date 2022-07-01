33e7d40df1a8544fbd93392a02fc21bb20f9cc15
Daniela Ruano <ruthdanielaruano@gmail.com>
2022-07-01 10:58:46 -0600

    :)

--- finalProject/africa0ViewController.swift
+++ finalProject/africa0ViewController.swift
@@ -9,8 +9,14 @@ import UIKit
 
 class africa0ViewController: UIViewController {
 
+    @IBOutlet weak var label1: UILabel!
+    
+    @IBOutlet weak var label2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        label1.text = "South Africa is one of the most beautiful countries in the world for its immense biodiversity, but the inequality in society is deeply marred. South Africa has one of the most unequal education systems in the world. These inequalities were highlighted to a greater extent during the Covid-19 pandemic. Approximately 60% of learning was lost for students living in socioeconomically disadvantaged communities. The few public education entities that rest do not have all the resources needed. In addition, one of the major challenges comes with the educators. Some South African public teachers could not pass simple Mathematics and English tests."
+        label2.text = "This is a major issue due to the fact that data published by Statistics South Africa at the start of June 2021, shows that the official unemployment rate among youth 15-34 years was 46.3%. Additionally, 7.2 million unemployed persons in the first quarter of 2021 had education levels below matric."
 
         // Do any additional setup after loading the view.
     }
---
+++ finalProject/trackerViewController.swift
@@ -0,0 +1,50 @@
+//
+//  trackerViewController.swift
+//  finalProject
+//
+//  Created by Daniela Ruano on 1/07/22.
+//
+
+import UIKit
+
+class trackerViewController: UIViewController {
+    
+    let stepper : UIStepper = {
+        let stepper = UIStepper()
+        stepper.minimumValue = -1
+        stepper.maximumValue = 21
+        stepper.stepValue = 1
+        
+        stepper.value = 0
+        
+        stepper.addTarget(self, action: #selector(update(_:)), for: .valueChanged)
+        
+        return stepper
+    }()
+    
+    let stepperValue: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stepperValue.text = "\(stepperValue)"
+        
+        view.addSubview(stepper)
+        stepper.center = view.center
+        
+        view.addSubview(stepperValue)
+        stepperValue.frame = CGRect(x: view.frame.size.width/2 - 50, y: view.frame.size.height/2 - 25 + 100, width: 100, height: 50)
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @objc func update(_ sender: UIStepper){
+        print(sender.value)
+        
+        stepperValue.text = "\(sender.value)"
+    }
+
+}
